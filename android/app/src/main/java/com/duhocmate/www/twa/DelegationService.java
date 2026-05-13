015976a355d4265c552e0f7cbde2c66737bc3f88
goz86 <goztrade86@gmail.com>
2026-05-13 22:13:48 +0900

    feat: Add Android TWA setup via Bubblewrap and assetlinks.json

---
+++ android/app/src/main/java/com/duhocmate/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.duhocmate.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
